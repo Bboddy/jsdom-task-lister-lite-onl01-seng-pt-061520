fce733679fb21ff34371387d116cf1883e0a0da7
Bryce B <bryce.boddy@gmail.com>
2021-01-05 12:00:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   function createNewTask() {
-  let ul = document.getElementById("tasks");
-  let li = document.createElement('li');
-  li.appendChild(document.createTextNode(name));
-  ul.appendChild(li);
-  li.innerHTML = document.getElementById("myDes").value;
-}
+    let ul = document.getElementById("tasks");
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(name));
+    ul.appendChild(li);
+    li.innerHTML = document.getElementById("myDes").value;
+  }
 });
