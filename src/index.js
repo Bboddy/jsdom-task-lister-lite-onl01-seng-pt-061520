235cccfd919b0fa267d78a73df2baeb4477f31f3
Bryce B <bryce.boddy@gmail.com>
2021-01-05 11:31:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskDis = document.getElementById("new-task-description");
   const taskList = document.getElementById("tasks");
 
-  document.getElementByid("create-task-form").addEventListener("click", function() {
+  document.getElementByValue("create-task-form").addEventListener("click", function() {
     var li = document.createElement("li");
     li.innerHTML = taskDis
     taskList.appendChild(li);
