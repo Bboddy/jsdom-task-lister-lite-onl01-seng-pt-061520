07015c8b56162e271af7d01fe7cc01e452fb745b
Bryce B <bryce.boddy@gmail.com>
2021-01-05 11:39:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
-  const taskDis = document.getElementById("new-task-description");
+  const taskDis = document.getElementById("new-task-description").innerHTML;
   const ul = document.getElementById("tasks");
 
   function createNewTask(){
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(name));
     ul.appendChild(li);
-    li.innerHTML = taskDis
+    li.innerHTML =
   }
 
   form.addEventListener('submit', createNewTask);
